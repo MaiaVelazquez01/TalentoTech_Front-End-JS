bf342020d1d7de32c4492b954daf79abba3669a1
unknown <mvelazquezyarbe@gmail.com>
2026-05-15 20:58:15 +0200

    clase 09 - ej práctico 2

--- Clase09/js/mi-script.js
+++ Clase09/js/mi-script.js
@@ -1,3 +1,4 @@
+alert("Ejercicio práctico #1");
 let num1 = prompt("Ingrese el primer número: ");
 while (isNaN(num1) || num1 === ""){
     num1 = prompt("Ingrese un número válido: ")
@@ -11,10 +12,31 @@ while (isNaN(num2) || num2 === ""){
 n1 = parseFloat(num1);
 n2 = parseFloat(num2);
 
-console.log("--------- OPERACIONES ---------");
+console.log("--- OPERACIONES CON VARIABLES Y TIPOS DE DATOS ---");
 console.log("n1 = " + n1 + " y n2 = " + n2);
 console.log("Suma: " + (n1 + n2));
 console.log("Resta: " + (n1 - n2));
 console.log("Multiplicación: " + (n1 * n2));
 console.log("División: " + (n1 / n2));
 console.log("Módulo: " + (n1 % n2));
+
+alert("Ejercicio práctico #2");
+
+let nombre = prompt("Ingrese nombre: ");
+while(!isNaN(nombre) || nombre == "") {
+    nombre = prompt("Ingrese nombre válido: ")
+}
+
+let edad = parseInt(prompt("Ingrese edad: "));
+while (isNaN(edad) || edad === "" || edad <= 0){
+    edad = parseInt(prompt("Ingrese edad válida: "))
+}
+
+console.log("--- CONCATENACIÓN Y CONVERSIÓN DE TIPOS DE DATOS ---");
+console.log("Nombre: " + nombre + " Edad: " + edad);
+console.log("Tu nombre es " + nombre + " y tenes " + edad + " años");
+if (edad >= 18) {
+    console.log("Sos mayor de edad")
+} else {
+    console.log("Sos menor de edad")
+}
