640b04b026843a957b72d99778151e963210f98c
unknown <mvelazquezyarbe@gmail.com>
2026-05-15 23:29:36 +0200

    clase 10 - ej práctico 1

---
+++ Clase10/js/mi-script.js
@@ -0,0 +1,10 @@
+let nombre = "Maia";
+let correo = "maia@gmail.com"
+let mensaje = "msj"
+
+console.log("------ Ejercicio práctico #1 ------");
+if (nombre !== "" && correo !== "" && mensaje !== "") {
+    console.log("Formulario completo. Listo para enviar.")
+} else {
+    console.log("Faltan completar campos obligatorios.")
+}
