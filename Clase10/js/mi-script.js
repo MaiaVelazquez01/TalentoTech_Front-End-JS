cd3ebe251c2897f5786dbbb95659a46b3a721885
unknown <mvelazquezyarbe@gmail.com>
2026-05-15 23:41:40 +0200

    clase 10 - ej práctico 2

--- Clase10/js/mi-script.js
+++ Clase10/js/mi-script.js
@@ -4,7 +4,14 @@ let mensaje = "msj"
 
 console.log("------ Ejercicio práctico #1 ------");
 if (nombre !== "" && correo !== "" && mensaje !== "") {
-    console.log("Formulario completo. Listo para enviar.")
+    console.log("Formulario completo. Listo para enviar.");
 } else {
-    console.log("Faltan completar campos obligatorios.")
+    console.log("Faltan completar campos obligatorios.");
 }
+
+console.log("------ Ejercicio práctico #2 ------");
+const productos = ['Crema corporal', 'Dentrífico', 'Desodorante', 'Shampoo', 'Acondicionador'];
+for (let producto of productos) {
+    alert(producto);
+}
+console.log("Lista de productos mostrada correctamente.")
